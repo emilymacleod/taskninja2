8a138b7d97c18c0e9855be17e0b72c457660a5c9
emilymacleod <margaretemacleod@gmail.com>
2015-04-12 12:23:33 -0300

    updated up to task 5
    updated up to task 5

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth) {
+app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth, Comment) {
 
 	$scope.searchTask = '';
 	$scope.tasks = Task.all;
@@ -8,6 +8,8 @@ app.controller('BrowseController', function($scope, $routeParams, toaster, Task,
 	$scope.signedIn = Auth.signedIn;
 	
 	$scope.listMode = true;
+
+	$scope.user = Auth.user;
 
 	if($routeParams.taskId) {
 		var task = Task.getTask($routeParams.taskId).$asObject();
@@ -22,6 +24,8 @@ app.controller('BrowseController', function($scope, $routeParams, toaster, Task,
 			$scope.isTaskCreator = Task.isCreator;
 			$scope.isOpen = Task.isOpen;
 		}
+
+		$scope.comments = Comment.comments(task.$id);
 	};
 
 	$scope.cancelTask = function(taskId) {
@@ -29,5 +33,17 @@ app.controller('BrowseController', function($scope, $routeParams, toaster, Task,
 			toaster.pop('success', "This task is cancelled successfully.");
 		});
 	};
+
+	$scope.addComment = function() {
+		var comment = {
+			content: $scope.content,
+			name: $scope.user.profile.name,
+			gravatar: $scope.user.profile.gravatar
+		};
+
+		Comment.addComment($scope.selectedTask.$id, comment).then(function(){
+			$scope.content = '';
+		});
+	}
 
 });
